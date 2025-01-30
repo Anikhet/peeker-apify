290a48d0fa0ab2771a1ebceb37f2c82ef15c6e55
Anikhet Mulky <am9559@g.rit.edu>
2025-01-30 00:23:39 -0800

    build fixes

--- src/app/payment-session/failure/page.tsx
+++ src/app/payment-session/failure/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
-import { Info } from "lucide-react";
+// import { Info } from "lucide-react";
 // import { createClient } from '@supabase/supabase-js';
 
 export default function PaymentSuccessPageComponent() {
